f03e6f6d3e53e3596a73bd8bef7b183a40cb340e
Nima Soufiani <n_soufiani@hotmail.com>
2024-08-06 23:23:47 +0100

    clean up server

--- backend-node/src/server.ts
+++ backend-node/src/server.ts
@@ -3,16 +3,11 @@ import { startStandaloneServer } from "@apollo/server/standalone"
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
   type Sleep {
     id: String
     sleepDuration: Float!
-    sleptAt: String
+    sleptAt: String!
   }
 
   type User {
@@ -22,15 +17,10 @@ const typeDefs = `#graphql
     sleeps: [Sleep!]!
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     users: [User!]!
   }
 `
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
         users: () => {
@@ -39,17 +29,11 @@ const resolvers = {
     },
 }
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 })
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } })
 
 console.log(`🚀 Server listening at: ${url}`)
