e32858510e5f896e456e7f6ce8af90c1df9d9d0e
Nima Soufiani <n_soufiani@hotmail.com>
2024-08-07 00:08:38 +0100

    confirm successful mutation of recordSleep

--- backend-node/src/server.ts
+++ backend-node/src/server.ts
@@ -14,11 +14,14 @@ const typeDefs = `#graphql
     id: String!
     name: String!
     gender: String!
-    sleeps: [Sleep!]!
+    sleeps: [Sleep]!
   }
 
   type Query {
     users: [User!]!
+  }
+  type Mutation {
+    recordSleep(name: String!, sleepDuration: Float, sleptAt: String!, gender: String!): User!
   }
 `
 const resolvers = {
@@ -27,6 +30,53 @@ const resolvers = {
             return prisma.user.findMany()
         },
     },
+    Mutation: {
+        recordSleep: async (_parent: unknown, args: RecordSleepInput) => {
+            console.log(args)
+            const { name, sleepDuration, sleptAt, gender } = args
+            const user = await prisma.user.findFirst({
+                where: {
+                    name,
+                },
+            })
+            if (user) {
+                const sleep = await prisma.sleep.create({
+                    data: {
+                        userId: user.id,
+                        sleepDuration,
+                        sleptAt,
+                    },
+                })
+                const allSleep = await prisma.sleep.findMany({
+                    where: {
+                        userId: user.id,
+                    },
+                })
+                return { ...user, sleeps: allSleep }
+            }
+            const newUser = await prisma.user.create({
+                data: {
+                    name,
+                    gender,
+                },
+            })
+            const sleep = await prisma.sleep.create({
+                data: {
+                    userId: newUser.id,
+                    sleepDuration,
+                    sleptAt,
+                },
+            })
+            return { ...newUser, sleeps: [sleep] }
+        },
+    },
+}
+
+interface RecordSleepInput {
+    name: string
+    sleepDuration: number
+    sleptAt: string
+    gender: string
 }
 
 const server = new ApolloServer({
