99cc0adf693dadb8a2fb7fbfbf6e1c568865f6f1
Nima Soufiani <n_soufiani@hotmail.com>
2024-08-10 22:20:50 +0100

    amend tabs in backend

--- backend-node/prisma/seed.ts
+++ backend-node/prisma/seed.ts
@@ -3,45 +3,45 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 const main = async () => {
-    const users: { name: string; gender: "Male" | "Female" | "Other" }[] = [
-        { name: "Carol Smith", gender: "Female" },
-        { name: "John Doe", gender: "Male" },
-    ]
+  const users: { name: string; gender: "Male" | "Female" | "Other" }[] = [
+    { name: "Carol Smith", gender: "Female" },
+    { name: "John Doe", gender: "Male" },
+  ]
 
-    users.map(async (user) => {
-        let date = new Date()
-        let dateUTC = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), 0, 0, 0, 0)
-        let dateTodayUTC = new Date(dateUTC)
+  users.map(async (user) => {
+    let date = new Date()
+    let dateUTC = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), 0, 0, 0, 0)
+    let dateTodayUTC = new Date(dateUTC)
 
-        const generatedUser = await prisma.user.upsert({
-            where: {
-                name: user.name,
-            },
-            update: {},
-            create: {
-                name: user.name,
-                gender: user.gender,
-            },
-        })
-        for (let i = 1; i < 8; i++) {
-            const pastDate = dateTodayUTC.setDate(dateTodayUTC.getDate() - 1)
-
-            await prisma.sleep.create({
-                data: {
-                    userId: generatedUser.id,
-                    sleepDuration: Math.round(Math.random() * (12 - 2) + 2),
-                    sleptAt: new Date(pastDate).toISOString(),
-                },
-            })
-        }
+    const generatedUser = await prisma.user.upsert({
+      where: {
+        name: user.name,
+      },
+      update: {},
+      create: {
+        name: user.name,
+        gender: user.gender,
+      },
     })
+    for (let i = 1; i < 8; i++) {
+      const pastDate = dateTodayUTC.setDate(dateTodayUTC.getDate() - 1)
+
+      await prisma.sleep.create({
+        data: {
+          userId: generatedUser.id,
+          sleepDuration: Math.round(Math.random() * (12 - 2) + 2),
+          sleptAt: new Date(pastDate).toISOString(),
+        },
+      })
+    }
+  })
 }
 
 main()
-    .catch((e) => {
-        console.log(e)
-        process.exit(1)
-    })
-    .finally(async () => {
-        await prisma.$disconnect()
-    })
+  .catch((e) => {
+    console.log(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
--- backend-node/src/server.ts
+++ backend-node/src/server.ts
@@ -31,79 +31,79 @@ const typeDefs = `#graphql
 `
 
 const resolvers = {
-    Query: {
-        users: async () => {
-            return await prisma.user.findMany({
-                include: {
-                    sleeps: {
-                        orderBy: {
-                            sleptAt: "asc",
-                        },
-                        skip: 0,
-                        take: -7,
-                    },
-                    _count: {
-                        select: {
-                            sleeps: true, // Have to query count like this to get all entries of sleep, otherwise we'll always get 7.
-                        },
-                    },
-                },
-            })
+  Query: {
+    users: async () => {
+      return await prisma.user.findMany({
+        include: {
+          sleeps: {
+            orderBy: {
+              sleptAt: "asc",
+            },
+            skip: 0,
+            take: -7,
+          },
+          _count: {
+            select: {
+              sleeps: true, // Have to query count like this to get all entries of sleep, otherwise we'll always get 7.
+            },
+          },
         },
+      })
     },
-    Mutation: {
-        recordSleep: async (_parent: unknown, args: RecordSleepInput) => {
-            const { name, sleepDuration, sleptAt, gender } = args
-            const user = await prisma.user.findFirst({
-                where: {
-                    name,
-                },
-            })
-            if (user) {
-                await prisma.sleep.create({
-                    data: {
-                        userId: user.id,
-                        sleepDuration,
-                        sleptAt,
-                    },
-                })
-                return await prisma.user.findFirst({
-                    where: {
-                        name,
-                    },
-                    include: {
-                        sleeps: true,
-                    },
-                })
-            }
-            const newUser = await prisma.user.create({
-                data: {
-                    name,
-                    gender,
-                },
-            })
-            await prisma.sleep.create({
-                data: {
-                    userId: newUser.id,
-                    sleepDuration,
-                    sleptAt,
-                },
-            })
-            return await prisma.user.findFirst({ where: { id: newUser.id }, include: { sleeps: true } })
+  },
+  Mutation: {
+    recordSleep: async (_parent: unknown, args: RecordSleepInput) => {
+      const { name, sleepDuration, sleptAt, gender } = args
+      const user = await prisma.user.findFirst({
+        where: {
+          name,
         },
+      })
+      if (user) {
+        await prisma.sleep.create({
+          data: {
+            userId: user.id,
+            sleepDuration,
+            sleptAt,
+          },
+        })
+        return await prisma.user.findFirst({
+          where: {
+            name,
+          },
+          include: {
+            sleeps: true,
+          },
+        })
+      }
+      const newUser = await prisma.user.create({
+        data: {
+          name,
+          gender,
+        },
+      })
+      await prisma.sleep.create({
+        data: {
+          userId: newUser.id,
+          sleepDuration,
+          sleptAt,
+        },
+      })
+      return await prisma.user.findFirst({ where: { id: newUser.id }, include: { sleeps: true } })
     },
+  },
 }
 
 interface RecordSleepInput {
-    name: string
-    sleepDuration: number
-    sleptAt: string
-    gender: string
+  name: string
+  sleepDuration: number
+  sleptAt: string
+  gender: string
 }
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 })
 
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } })
