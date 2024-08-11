9030e95a17f02603966aa6f3b4a4050bce0e199a
Nima Soufiani <n_soufiani@hotmail.com>
2024-08-11 07:43:09 +0100

    refactor recordSleep resolver

--- backend-node/src/resolvers/recordSleep.ts
+++ backend-node/src/resolvers/recordSleep.ts
@@ -2,31 +2,36 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
+const getUser = async (name: string) => {
+  return await prisma.user.findFirst({
+    where: {
+      name,
+    },
+    include: {
+      sleeps: true,
+    },
+  })
+}
+
+const createSleepRecord = async (userId: string, sleepDuration: number, sleptAt: string) => {
+  await prisma.sleep.create({
+    data: {
+      userId,
+      sleepDuration,
+      sleptAt,
+    },
+  })
+}
+
 const resolvers = {
   Mutation: {
     recordSleep: async (_parent: unknown, args: RecordSleepInput) => {
       const { name, sleepDuration, sleptAt, gender } = args
-      const user = await prisma.user.findFirst({
-        where: {
-          name,
-        },
-      })
+
+      const user = await getUser(name)
       if (user) {
-        await prisma.sleep.create({
-          data: {
-            userId: user.id,
-            sleepDuration,
-            sleptAt,
-          },
-        })
-        return await prisma.user.findFirst({
-          where: {
-            name,
-          },
-          include: {
-            sleeps: true,
-          },
-        })
+        await createSleepRecord(user.id, sleepDuration, sleptAt)
+        return await getUser(name)
       }
       return prisma.$transaction(async (tx) => {
         const newUser = await prisma.user.create({
@@ -35,14 +40,8 @@ const resolvers = {
             gender,
           },
         })
-        await prisma.sleep.create({
-          data: {
-            userId: newUser.id,
-            sleepDuration,
-            sleptAt,
-          },
-        })
-        return await prisma.user.findFirst({ where: { id: newUser.id }, include: { sleeps: true } })
+        await createSleepRecord(newUser.id, sleepDuration, sleptAt)
+        return await getUser(newUser.name)
       })
     },
   },
